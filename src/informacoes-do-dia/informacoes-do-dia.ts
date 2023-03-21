60453ff5c08ea3796d11fb45f747a87b8fc88f9a
carlosladeira <47091965+carlosladeira@users.noreply.github.com>
2023-03-21 12:33:26 -0300

    Some style and logic

--- src/informacoes-do-dia/informacoes-do-dia.ts
+++ src/informacoes-do-dia/informacoes-do-dia.ts
@@ -1,14 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'informacoes-do-dia',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './informacoes-do-dia.html'
+  templateUrl: './informacoes-do-dia.html',
+  styleUrls: ['./informacoes-do-dia.css']
 })
-export class InformacoesDoDia {
+export class InformacoesDoDia implements OnInit {
   @Input() dia: string;
   @Input() nomeDiaria: string;
   @Input() nomePernoite: string;
+
+  adverbioTempo: string;
+  hoje = new Date().toLocaleDateString();
+
+  ngOnInit() {
+    console.log('Dia: ', this.dia);
+    console.log('Hoje: ', this.hoje);
+
+    var now = new Date();
+    var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+    console.log(days[ now.getDay() ]);
+
+    this.adverbioTempo = this.dia === this.hoje ? 'HOJE' : 'AMANHÃ';
+    //console.log(this.adverbioTempo)
+  }
 }
