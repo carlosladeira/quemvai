1685f739307840ddebdaed5c4876fbf379831df6
carlosladeira <47091965+carlosladeira@users.noreply.github.com>
2023-03-21 12:06:06 -0300

    Component creation

---
+++ src/informacoes-do-dia/informacoes-do-dia.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'informacoes-do-dia',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './informacoes-do-dia.html'
+})
+export class InformacoesDoDia {
+}
--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,18 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { InformacoesDoDia } from './informacoes-do-dia/informacoes-do-dia';
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InformacoesDoDia],
   template: `
     <h1>Hello from {{name}}!</h1>
     <a target="_blank" href="https://angular.io/start">
       Learn more about Angular 
     </a>
+    <informacoes-do-dia />
   `,
 })
 export class App {
